fcc8c1bc4ad6604528ea951abc4d2cd402b50e94
ReadyDoMe <unitecomrades777@gmail.com>
2019-01-22 23:22:55 -0600

    installed yargs, encoded user input to get dynamic location results

--- app.js
+++ app.js
@@ -1,10 +1,30 @@
 const request = require('request');
+const yargs = require('yargs');
+
 const keys = require('./apiKeys');
 
+//Use yargs to accept address from terminal
+const argv = yargs
+  .options({
+    a: {
+      demand: true,
+      alias: 'address',
+      describe: 'Address to fetch weather for',
+      string: true,
+    },
+  })
+  .help()
+  .alias('help', 'h')
+  .argv;
+
+//Encode address from yargs
+const encodedAddress = encodeURIComponent(argv.address);
+
 request({
-  url: `http://www.mapquestapi.com/geocoding/v1/address?key=${keys.mapQuestKey}&location=1301%20lombard%20street%20philadelphia`,
+  url: `http://www.mapquestapi.com/geocoding/v1/address?key=${keys.mapQuestKey}&location=${encodedAddress}`,
   json: true,
 }, (error, response, body)=> {
+  console.log('Address', response.body.results[0].providedLocation.location);
   console.log('latitude', body.results[0].locations[0].latLng.lat);
-  console.log('longitude' ,body.results[0].locations[0].latLng.lng)
-})
+  console.log('longitude' , body.results[0].locations[0].latLng.lng)
+});
