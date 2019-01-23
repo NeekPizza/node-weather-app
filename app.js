21f592ad33272b109ff357c34cfded075a5035e1
ReadyDoMe <unitecomrades777@gmail.com>
2019-01-22 23:42:07 -0600

    added error handling

--- app.js
+++ app.js
@@ -24,7 +24,14 @@ request({
   url: `http://www.mapquestapi.com/geocoding/v1/address?key=${keys.mapQuestKey}&location=${encodedAddress}`,
   json: true,
 }, (error, response, body)=> {
-  console.log('Address', response.body.results[0].providedLocation.location);
-  console.log('latitude', body.results[0].locations[0].latLng.lat);
-  console.log('longitude' , body.results[0].locations[0].latLng.lng)
+  console.log(body);
+  if (error) {
+    console.log('Unable to connect to servers.');
+  } else if (body.results[0].locations.length === 0) {
+    console.log('Unable to find that address');
+  } else if (response.statusCode === 200) {
+    console.log('Address', body.results[0].providedLocation.location);
+    console.log('latitude', body.results[0].locations[0].latLng.lat);
+    console.log('longitude' , body.results[0].locations[0].latLng.lng);
+  }
 });
