71f9918d5ec86dd736139e98dd1d908a0cd92f8e
ReadyDoMe <unitecomrades777@gmail.com>
2019-01-23 22:56:55 -0600

    extracted functionality for geocoding location, and handling api errors

--- app.js
+++ app.js
@@ -1,7 +1,5 @@
-const request = require('request');
 const yargs = require('yargs');
-
-const keys = require('./apiKeys');
+const geocode = require('./geocode/geocode.js');
 
 //Use yargs to accept address from terminal
 const argv = yargs
@@ -17,21 +15,11 @@ const argv = yargs
   .alias('help', 'h')
   .argv;
 
-//Encode address from yargs
-const encodedAddress = encodeURIComponent(argv.address);
+  geocode.geocodeAddress(argv.address, (errorMessage, results) => {
+    if (errorMessage) {
+      console.log(errorMessage);
+    } else {
+      console.log(JSON.stringify(results, undefined, 2));
+    }
+  });
 
-request({
-  url: `http://www.mapquestapi.com/geocoding/v1/address?key=${keys.mapQuestKey}&location=${encodedAddress}`,
-  json: true,
-}, (error, response, body)=> {
-  console.log(body);
-  if (error) {
-    console.log('Unable to connect to servers.');
-  } else if (body.results[0].locations.length === 0) {
-    console.log('Unable to find that address');
-  } else if (response.statusCode === 200) {
-    console.log('Address', body.results[0].providedLocation.location);
-    console.log('latitude', body.results[0].locations[0].latLng.lat);
-    console.log('longitude' , body.results[0].locations[0].latLng.lng);
-  }
-});
---
+++ geocode/geocode.js
@@ -0,0 +1,28 @@
+const request = require('request');
+
+const keys = require('../apiKeys');
+
+const geocodeAddress = (address, callback) => {
+  const encodedAddress = encodeURIComponent(address);
+  
+  request({
+    url: `http://www.mapquestapi.com/geocoding/v1/address?key=${keys.mapQuestKey}&location=${encodedAddress}`,
+    json: true,
+  }, (error, response, body)=> {
+    if (error) {
+      callback('Unable to connect to servers.');
+    } else if (body.results[0].locations.length === 0) {
+      callback('Unable to find that address');
+    } else if (response.statusCode === 200) {
+      callback(undefined, {
+        address: body.results[0].providedLocation.location,
+        latitude: body.results[0].locations[0].latLng.lat,
+        longitude:body.results[0].locations[0].latLng.lng,
+      });
+    }
+  });
+};
+
+module.exports = {
+  geocodeAddress,
+};
